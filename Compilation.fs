e0989a038cb0403e09fd39157aecb28d8d68ac0c
Nickolas Lloyd <smindinvern@users.noreply.github.com>
2020-02-09 18:27:55 -0500

    Use State monad to represent "compiled" code

--- Compilation.fs
+++ Compilation.fs
@@ -7,27 +7,48 @@ module Compilation
     open System
 
     open smindinvern.State.Lazy
+    open smindinvern.Utils
     
     type CompilerState = { Bindings: Scope }
-    type Compiled = State<CompilerState, Scope -> (Scope * LispData)>
+    type Compiled = State<CompilerState, State<Scope, LispData>>
 
     open Parsing
     
+    let discard (s: State<'s, 'a>) =
+        state {
+            let! original = get
+            let! a = s
+            do! put original
+            return a
+        }
+    
     // Evaluate a list of expressions in sequence, preserving modifications made to the
     // dynamic environment.
-    let sequenceExpressions (cexprs: (Scope -> (Scope * LispData)) list) (scope: Scope) =
-        List.fold
-            // Discard result of previous expression and evaluate the next one, threading
-            // the current scope through the evaluation.
-            (fun (scope, _) cexpr -> cexpr scope)
-            // Start with the initial scope.  If the list is empty return 'nil.
-            (scope, Symbol "nil")
-            cexprs
+    let sequenceExpressions cexprs : State<Scope, LispData> =
+        //foldM (flip konst) (Symbol "nil") cexprs
+        state {
+            let! scope = get
+            let (scope', result) =
+                List.fold
+                    // Discard result of previous expression and evaluate the next one, threading
+                    // the current scope through the evaluation.
+                    (fun (scope, _) cexpr -> runState cexpr scope)
+                    // Start with the initial scope.  If the list is empty return 'nil.
+                    (scope, Symbol "nil")
+                    cexprs
+            do! put scope'
+            return result
+        }
 
     // Evaluate a list of expressions in sequence, discarding modifications made to the
     // dynamic environment.
-    let evalExpressions (cexprs: (Scope -> (Scope * LispData)) list) (scope: Scope) =
-        List.map (snd << ((|>) scope)) cexprs
+    let evalExpressions (cexprs: State<Scope, LispData> list) =
+        //sequence <| List.map discard cexprs
+        state {
+            let! scope = get
+            let results = List.map (snd << (flip runState <| scope)) cexprs
+            return results
+        }
     
     // Extract and return all Bindings from a Pattern as a list of key-value pairs.
     let getPatternBindings (ps: Pattern list) =
@@ -48,19 +69,25 @@ module Compilation
         | Option.None ->
             failwith "Binding failed"
     
-    let addToScope (scope: Scope) (bindings: Binding list) =
+    let addToScope (bindings: Binding list) (scope: Scope) =
         let bindings = List.map (fun (b: Binding) -> (b.sym, b)) bindings
         scope.FreshScope().AddRange(bindings)
     
-    let runtimeLookup (b: Binding) (scope: Scope) =
-        // First attempt to resolve the compile-time reference.
-        match !b.ldr with
-        | Option.Some(v) -> (scope, v)
-        | Option.None ->
-            // Attempt to lookup @s in the dynamic environment.  This is used only for lambda-captured values.
-            match scope.LookupValue(b.sym) with
-            | Option.Some(v) -> (scope, v)
-            | Option.None -> failwithf "Compiler error: variable `%s' has not yet been assigned." b.sym
+    let runtimeLookup (b: Binding) : State<Scope, LispData> =
+        state {
+            // This needs to go first.  Otherwise the match expression below will be executed
+            // outside of the computation expression.
+            let! scope = get
+            // First attempt to resolve the compile-time reference.
+            match !b.ldr with
+            | Option.Some(v) -> return v
+            | Option.None ->
+                // Attempt to lookup @s in the dynamic environment.  This is used only for lambda-captured values.
+                match scope.LookupValue(b.sym) with
+                | Option.Some(v) -> return v
+                | Option.None ->
+                    return failwithf "Compiler error: variable `%s' has not yet been assigned." b.sym
+        }
     
     let rec compileExpr (e: Expr) : Compiled =
         match e with
@@ -70,11 +97,11 @@ module Compilation
                     match st.Bindings.Lookup(s) with
                     | Some(b) ->
                         match !b.ldr with
-                        | Option.Some(v) -> return (fun scope -> (scope, v))
+                        | Option.Some(v) -> return inject v
                         | Option.None -> return runtimeLookup b // Lazy initialization.
                     | None -> return failwithf "Attempt to use free variable: %s" s
                 }
-            | LiteralExpr l -> inject <| fun x -> (x, l)
+            | LiteralExpr l -> inject <| inject l
             | ListExpr es ->
                 compileListExpr es
             | LetExpr (bindings, es) -> compileLetExpr (bindings, es)
@@ -82,12 +109,14 @@ module Compilation
             | IfExpr (test, ifTrue, ifFalse) -> compileIfExpr test ifTrue ifFalse
             | QuotedExpr q -> compileQuoted q
             | ConsExpr (head, tail) ->
-                let f hc tc scope =
-                    let (scope, h) = hc scope
-                    let (scope, t) = tc scope
-                    match t with
-                        | Ast.List xs -> (scope, Ast.List (h::xs))
-                        | _ -> failwith <| sprintf "Cannot cons %A and %A" h t
+                let f hc tc =
+                    state {
+                        let! h = hc
+                        let! t = tc
+                        match t with
+                        | Ast.List xs -> return Ast.List(h::xs)
+                        | _ -> return failwithf "Cannot cons %A and %A" h t
+                    }
                 state {
                     let! hc = compileExpr head
                     let! tc = compileExpr tail
@@ -97,11 +126,13 @@ module Compilation
                 compileLambda parms body
     and compileLetExpr (bindings: LetBinding list, es: Expr list) : Compiled =
         let (patterns, exprs) = List.unzip bindings
-        let bindAtRuntime exprs body scope =
-            let exprs = evalExpressions exprs scope
-            ignore <| bindAll patterns exprs
-            let (_, ld) = sequenceExpressions body scope
-            (scope, ld)
+        let bindAtRuntime exprs body =
+            state {
+                let! exprs = evalExpressions exprs
+                // NB: Mutating state.
+                do ignore <| bindAll patterns exprs
+                return! sequenceExpressions body
+            }
         state {
             let! exprs' = sequence <| List.map compileExpr exprs
             let! s = get
@@ -119,36 +150,45 @@ module Compilation
             return (p, result)
         }
     and compileCaseExpr (e: Expr) (arms: (Pattern * Expr list) list) =
-        let rec eval (scope: Scope) (v: LispData) (carms: (Pattern * (Scope -> Scope * LispData) list) list) =
-            // try binding @e to each pattern in @arms until we find a match
-            match carms with
-            | (p, cbody)::carms' ->
-                match bindPattern p v with
-                | Option.Some(bindings) ->
-                    let (_, result) = sequenceExpressions cbody (addToScope scope bindings)
-                    (scope, result)
-                | Option.None -> eval scope v carms'
-            | [] -> failwith "No match in case expression"
+        let rec eval (v: LispData) (carms: (Pattern * State<Scope, LispData> list) list) =
+            state {
+                let! s = get
+                // try binding @e to each pattern in @arms until we find a match
+                match carms with
+                | (p, cbody)::carms' ->
+                    match bindPattern p v with
+                    | Option.Some(bindings) ->
+                        do! modify (fun scope -> addToScope bindings scope)
+                        let! result = sequenceExpressions cbody
+                        do! put s
+                        return result
+                    | Option.None -> return! eval v carms'
+                | [] -> return failwith "No match in case expression"
+            }
         state {
             let! carms = sequence <| List.map compileCaseArm arms
             let! ce = compileExpr e
-            return fun scope ->
-                let (_, v) = ce scope
-                eval scope v carms
+            return state {
+                let! v = discard ce
+                return! eval v carms
+            }
         }
     and compileListExpr (es: Expr list) : Compiled =
         state {
             let! cs = sequence <| List.map compileExpr es
-            return fun scope ->
-                match evalExpressions cs scope with
-                | [] -> (scope, Ast.List [])
+            return state {
+                let! scope = get
+                let! evaled = evalExpressions cs
+                match evaled with
+                | [] -> return Ast.List []
                 | (Symbol f)::args ->
                     match scope.LookupValue(f) with
-                        | Some(LispFunc func) -> (scope, func.Invoke(args))
-                        | _ -> failwith <| sprintf "%s is not a callable object" f
+                        | Some(LispFunc func) -> return func.Invoke(args)
+                        | _ -> return failwith <| sprintf "%s is not a callable object" f
                 | (LispFunc func)::args ->
-                    (scope, func.Invoke(args))
-                | x::_ -> failwith <| sprintf "%A is not a callable object" x
+                    return func.Invoke(args)
+                | x::_ -> return failwith <| sprintf "%A is not a callable object" x
+            }
         }
     and compileIfExpr (test: Expr) (ifTrue: Expr) (ifFalse: Expr option) : Compiled =
         state {
@@ -159,14 +199,16 @@ module Compilation
                     | None -> SymbolExpr "nil")
             let! ifTrue' = compileExpr ifTrue
             let! test' = compileExpr test
-            return fun scope ->
-                if (snd <| test' scope) <> (Symbol "nil") then
-                    ifTrue' scope
+            return state {
+                let! result = discard test'
+                if result <> (Symbol "nil") then
+                    return! ifTrue'
                 else
-                    ifFalse' scope
+                    return! ifFalse'
+            }
         }
     and compileQuoted (q: LispData) : Compiled =
-        inject <| fun scope -> (scope, q)
+        inject <| inject q
     and compileClosure (captureList: string list) =
         state {
             let! s = get
@@ -177,8 +219,11 @@ module Compilation
             // Temporarily insert empty bindings for captured variables so that they won't be
             // resolved to anything, forcing a lookup in the dynamic environment.
             let empties = List.map (fun sym -> Binding(sym)) captureList
-            do! put { s with Bindings = addToScope s.Bindings empties }
-            return fun (scope: Scope) ->
+            do! put { s with Bindings = addToScope empties s.Bindings }
+            return state {
+                // This needs to go first.  Otherwise the let-binding below will be executed
+                // outside of the computation expression.
+                let! (scope: Scope) = get
                 // Make a copy of each binding.
                 let copies = List.map (fun (b: Binding) ->
                     // Check if this variable is captured by an outer lambda.
@@ -188,19 +233,23 @@ module Compilation
                     // Otherwise, create a copy of the binding that refers to a different location.
                     | Option.None -> Binding(b.sym, !b.ldr)) captured
                 // Insert into the dynamic environment.
-                addToScope scope copies
+                do! put (addToScope copies scope)
+            }
         }
-    and compileFunction (paramList: Pattern list) (body: Expr list) =
+    and compileFunction (paramList: Pattern list) (body: Expr list) : State<CompilerState, State<Scope, LispData>> =
         state {
             let! s = get
             do! addPatternBindings paramList
             let! cbody = sequence <| List.map compileExpr body
             do! put s
             let bindArgs (scope: Scope) (args: LispData list) =
-                ignore <| bindAll paramList args
-                snd <| sequenceExpressions cbody scope
-            return fun scope ->
-                (scope, LispFunc (Func<LispData list, LispData>(bindArgs scope))) 
+                // NB: Mutating state.
+                do ignore <| bindAll paramList args
+                snd <| runState (sequenceExpressions cbody) scope
+            return state {
+                let! scope = get
+                return LispFunc (Func<LispData list, LispData>(bindArgs scope))
+            }
         }
     and compileLambda (paramList: Pattern list) (body: Expr list) =
         let (_, freeVars) = findFreeVars (ListExpr body) []
@@ -211,10 +260,14 @@ module Compilation
             let! closure = compileClosure freeVars
             let! cfunc = compileFunction paramList body
             do! put s
-            return fun scope ->
+            return state {
+                let! scope = get
                 // Capture @freeVars
-                let newScope = closure scope
-                (scope, snd <| cfunc newScope)
+                do! closure
+                let! result = cfunc
+                do! put scope
+                return result
+            }
         }
         
     let compileDefun (sym: string, paramList: ParamList, body: Expr list) : Compiled =
@@ -224,13 +277,15 @@ module Compilation
             let! s = get
             do! put { s with Bindings = s.Bindings.Add(sym, b) }
             let! cfunc = compileFunction paramList body
-            return fun scope ->
+            return state {
                 // Insert binding into the dynamic scope so that it can be looked up and invoked.
-                let scope = scope.Add(b.sym, b)
-                let (scope', cfunc') = cfunc <| scope
+                do! modify (fun (scope: Scope) -> scope.Add(b.sym, b))
+                let! cfunc' = cfunc
                 // Update the binding to resolve to the compiled function.
+                // NB: Mutating state.
                 b.ldr := Option.Some(cfunc')
-                (scope', cfunc')
+                return cfunc'
+            }
         }
         
     open Environment
@@ -242,5 +297,5 @@ module Compilation
             (sym, Binding(sym, ld))
         let environment = List.map f Builtins
         let compiled = sequence <| List.map compileDefun t
-        let (x, compiled) = runState compiled { Bindings = Scope([dict environment]) } 
-        List.fold (fun s c -> fst <| c s) scope compiled
+        let (x, compiled) = runState compiled { Bindings = Scope([dict environment]) }
+        fst <| runState (sequence compiled) scope
